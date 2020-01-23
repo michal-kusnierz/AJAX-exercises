b82da19b7bce938645e2061b104c5fc174e903b6
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-23 20:09:07 +0100

    map() method uses a callback function

--- script.js
+++ script.js
@@ -1,29 +1,9 @@
-function mult(x, y) {
-  return x * y;
-}
+const array = [1, 2, 3, 4, 5];
 
-function add(x, y) {
-  return x + y;
-}
-
-function calculate(x, y, compute) {
-  return compute(x, y);
-}
-
-let a = calculate(10, 5, add);
-console.log(a);
-
-let b = calculate(10, 5, mult);
-console.log(b);
-
-// anonymous callback:
-let c = calculate(10, 5, function(x, y) {
-  return x - y; //subtracts y from x
+const newArray = array.map(x => {
+  //uses an anonymous callback function to square each element
+  return x * x;
 });
-console.log(c); // logs 5
 
-// anonymous using arrow function
-let d = calculate(10, 5, (x, y) => {
-  return x - y;
-});
-console.log(d);
+console.log(newArray);
+// logs [1,4,9,16,25]
