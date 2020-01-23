f63caddbde3c7d357700b99cb5a352adfb396956
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-23 20:28:17 +0100

    chaining callbacks with Continuation Passing Style (CPS) - example in ES5

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const array = [1,2,3,4,5];
+/*
+The Continuation Passing Style(CPS)
+ is a programming style used to chain callback functions together. 
+ In CPS, methods with callback functions as arguments are called within other callback functions. CPS is characterized by having methods that have callback functions as their last argument.
+*/
 
-function isEven(x){ //checks if a value is even
-   return x % 2 == 0; 
+function myFunction(x,callback){
+  callback(x);
 }
 
-const newArray = array.filter(isEven); //uses a callback to check if an element is even
+var answer = 0;
 
-console.log(newArray);
-// logs [2,4]
+myFunction(10,function(x){ //callback1
+  var result = x * x; //result = 100
+
+  myFunction(result, function(x){ //callback2 within callback 1
+      var result2 = x + x; //result2 = 200
+
+      myFunction(result2, function(x){ //callback 3 within callback 2
+          answer = x + 100;
+          console.log(answer); // logs 300
+      })
+  })
+});
