fcac55a9af3ad7510fe45271f1a8cb5b566e0d24
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-23 19:40:32 +0100

    how  asynchronous code prevents slow tasks from blocking other faster tasks

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('initial "hi!"')
+const slowTask = () => {
+  console.log("slow task finished");
+};
+
+const asyncSlowTask = val => {
+  setTimeout(slowTask, 2000);
+};
+
+const fastTask = () => {
+  console.log("fast task finished!");
+};
+
+fastTask();
+asyncSlowTask();
+fastTask();
+asyncSlowTask();
+fastTask();
+fastTask();
