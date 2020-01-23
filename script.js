641b0df0925766ac0f138655a4731ed2bba2d368
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-23 23:35:27 +0100

    handling keypress event with addEventListener method

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ document.getElementById("myButton").onclick = () => {
   value++;
   document.getElementById("myButton").innerHTML = value;
 };
+
+
+document.addEventListener('keypress',handleKeyPress);
+
+function handleKeyPress(event){
+    var keyPressed= event.key; //event.key contains the key that was pressed
+    document.getElementById("text").innerHTML = "Key Pressed: " +  keyPressed;
+    //sets the HTML text to display the key pressed
+} 
