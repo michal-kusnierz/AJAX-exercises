90df22b8049e7a60318b3f86b8932c4e59f3863d
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-28 10:12:07 +0100

    added .js file to extended game version

---
+++ memoryMatch16.js
@@ -0,0 +1,109 @@
+let clickedArray = [];
+let interval;
+let started = false;
+let time = 0;
+let ready = true;
+let numCompleted = 0;
+
+const randomAnswers = () => {
+  const answers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16];
+  answers.sort(item => {
+    return 0.5 - Math.random();
+  });
+  return answers;
+};
+
+const reveal = cell => {
+  cell.style.backgroundColor = "red";
+  cell.innerHTML = cell.value;
+  cell.clicked = true;
+};
+
+const hide = cell => {
+  cell.style.backgroundColor = "blue";
+  cell.innerHTML = "";
+  cell.clicked = false;
+};
+
+const complete = cell => {
+  numCompleted++;
+  cell.completed = true;
+  cell.style.backgroundColor = "purple";
+};
+
+const startTimer = () => {
+  if (started == false) {
+    interval = setInterval(() => {
+      time++;
+      document.getElementById("timer").innerHTML = "Time Elapsed: " + time;
+    }, 1000);
+    started = true;
+  }
+};
+
+const setUp = () => {
+  const grid = document.getElementsByTagName("td");
+  const answers = randomAnswers();
+
+  for (let i = 0; i < grid.length; i++) {
+    let cell = grid[i];
+    cell.completed = false;
+    cell.clicked = false;
+    cell.value = answers[i];
+
+    cell.addEventListener("mouseenter", function() {
+      if (this.completed == false && this.clicked == false)
+        this.style.background = "orange";
+    });
+    cell.addEventListener("mouseleave", function() {
+      if (this.completed == false && this.clicked == false)
+        this.style.background = "blue";
+    });
+    cell.addEventListener("click", function() {
+      if (ready == false) return;
+      if (this.clicked == false && this.completed == false) {
+        clickedArray.push(this);
+        startTimer();
+        reveal(this);
+      }
+      if (clickedArray.length == 2) {
+        if (clickedArray[0].value == clickedArray[1].value) {
+          complete(clickedArray[0]);
+          complete(clickedArray[1]);
+          clickedArray = [];
+
+          if (numCompleted == 8) {
+            alert("You won in " + time + " seconds!");
+            clearInterval(interval);
+          }
+        } else {
+          ready = false;
+          document.getElementById("gridTable").style.border = "5px solid red";
+
+          setTimeout(() => {
+            hide(clickedArray[0]);
+            hide(clickedArray[1]);
+
+            clickedArray = [];
+
+            ready = true;
+            document.getElementById("gridTable").style.border =
+              "5px solid black";
+          }, 500);
+        }
+      }
+    });
+  }
+
+  document.addEventListener("keydown", event => {
+    if (event.key > 0 && event.key < 10) {
+      grid[event.key - 1].click();
+    }
+  });
+
+  document.getElementById("restart").addEventListener("click", () => {
+    location.reload();
+  });
+};
+
+setUp();
