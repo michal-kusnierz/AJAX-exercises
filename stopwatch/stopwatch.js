effbcbc0862144695a04dc76255442ba6bf84791
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-29 13:19:56 +0100

    take1: how intervals work  - start and stop functions

--- stopwatch/stopwatch.js
+++ stopwatch/stopwatch.js
@@ -5,3 +5,21 @@ const timer = document.getElementById('timer');
 const startBtn = document.getElementById('start');
 const resetBtn = document.getElementById('reset');
 const recordBtn = document.getElementById('record');
+
+const clock = () => {
+  time++;
+  timer.innerHTML = time;
+}
+
+const startTimer = () => {
+  interval = setInterval(clock, 10);
+  startBtn.removeEventListener('click',startTimer);
+  startBtn.addEventListener('click', stopTimer);
+}
+
+const stopTimer = () => {
+  clearInterval(interval);
+  startBtn.addEventListener('click', startTimer);
+}
+
+startBtn.addEventListener('click', startTimer);
