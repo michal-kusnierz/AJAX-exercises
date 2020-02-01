81783c6ac57b35464cf4d85d9f7cd936763fa23e
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-01 15:15:44 +0100

    promises with then() and catch() methods

--- promises.js
+++ promises.js
@@ -7,13 +7,24 @@ const promise = new Promise(function(resolve, reject) {
   //do stuff
 
   let isSuccessful = true;
-
-  if (isSuccessful) { /*if everything is successful*/
+  setTimeout(function(){
+    if (isSuccessful) { //if everything is successful
       resolve("Success!");
-  }
-  else {              /*if something went wrong*/
-      reject(Error("Failure."));
-  }
+    }
+    else {              //if something went wrong
+        reject(Error("Failure."));
+    }
+  },5000);
+
+});
+
+//promise status changes from pending to resolved after 5 seconds
+promise.then(function(val){ //val represents the fulfillment value
+  console.log(val);         //logs "success!" since promise resolved
+
+}).catch(function(val){     //val represents the rejection value
+  console.log(val);         //doesn't occur since promise never rejected
+
 });
 
 /*
