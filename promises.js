9c037e872c12d8d2d11512baf8853b4a186e9c70
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-02 13:43:35 +0100

    promise in ES6 syntax

--- promises.js
+++ promises.js
@@ -1,33 +1,18 @@
-/*
-The new Promise() constructor is called to create a new promise. 
-The constructor takes in a callback function with the arguments resolve and reject.
-*/
-const promise = new Promise(function(resolve, reject) {
-
-  //do stuff
-
-  let isSuccessful = true;
+const promise = new Promise((resolve, reject) => {
   setTimeout(function(){
-    if (isSuccessful) { //if everything is successful
-      resolve("Success!");
+    const val = Math.random();
+    if (val > 0.5){
+      resolve(val);
     }
-    else {              //if something went wrong
-        reject(Error("Failure."));
+    else {
+      reject(val);
     }
   },5000);
-
 });
 
-//promise status changes from pending to resolved after 5 seconds
-promise.then(function(val){ //val represents the fulfillment value
-  console.log(val);         //logs "success!" since promise resolved
+promise.then((val) => {
+  console.log('success: '+ val);
 
-},function(val){          //rejection callback
-  console.log(val);         //doesn't occur since promise never rejected
-
+}).catch((err)=>{
+  console.log('error: ' + err);
 });
-
-/*
-The then() method can be called with a success callback and a failure callback 
-as an alternative to using the then() and catch() methods. 
-*/
