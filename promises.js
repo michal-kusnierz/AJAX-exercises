1ddd886a04095ce6464a5cd45b6537ddce9b24a7
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-02 22:15:27 +0100

    appendChild paragraph with innerHTML promise  - either success or failure depending on Math.random

--- promises.js
+++ promises.js
@@ -1,8 +1,12 @@
+const promiseDiv = document.getElementById('promise');
 const promiseBtn = document.getElementById('promiseBtn');
-
+const promiseDisplay = document.createElement('p');
+    
 const promiseFun = () => {
   const promise = new Promise((resolve, reject) => {
-    promiseBtn.innerHTML='waiting...';
+    promiseDisplay.innerHTML='waiting...';
+    promiseDiv.appendChild(promiseDisplay);
+
     setTimeout(function(){
       const val = Math.random();
       if (val > 0.5){
@@ -16,9 +20,11 @@ const promiseFun = () => {
 
   promise.then((val) => {
     console.log('success: '+ val);
+    promiseDisplay.innerHTML = 'success: ' + Math.ceil(val);
 
   }).catch((err)=>{
     console.log('error: ' + err);
+    promiseDisplay.innerHTML = 'error: ' + Math.floor(err);
   });
 }
 
