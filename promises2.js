1bd75a764c7623cc9768dd7b76971e80f00af8d9
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-03 13:37:48 +0100

    returning a Promise within a then() callback creates a new promise

--- promises2.js
+++ promises2.js
@@ -1,25 +1,17 @@
 /*
-Several transforms can be chained together using multiple then() method calls.
+Returning another Promise within a then() callback 
+will cause the then() method to return the returned Promise.
 */
 
-const promise = Promise.resolve([1,2,3,4]);
+const promise = Promise.resolve("hello");
 
-promise.then((result)=>{ 
+const promise2 = promise.then((result)=>{ 
     console.log(result) //logs "hello"
-    return result.map(x => x * x) //squares each value in the array
+    return Promise.resolve("12345") 
+                            //causes then() to return a promise with a fulfillment value of "12345"
 
-}).then(function(result2){
-  console.log(result2) //logs [1,4,9,16]
-  return result2.filter( x => x > 10); //filters out elements that are not larger than 10
+});
 
-}).then(function(result3){
-  console.log(result3) //logs [16]
-  return result3.toString() + "!!"; //converts result3 to a string and adds "!!"
-
-}).then(function(result4){
-  console.log(result4) //logs "16!!"
-  return result4;  //returns a promise with "16!!" as the fulfillment value
-
-}).catch(function(error){
-  console.log(error)
+promise2.then((result)=>{
+    console.log(result);    //logs "12345"
 });
