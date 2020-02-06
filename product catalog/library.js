b72d8afa9b2657370ad46c15b47371b148faf144
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-06 23:55:55 +0100

    added library.js file

---
+++ product catalog/library.js
@@ -0,0 +1,22 @@
+((window)=>{
+
+  const myLibrary=()=>{
+
+      //execute code here
+
+      return {
+          searchProductById: searchProductById,
+          searchProductsByPrice: searchProductsByPrice,
+          searchProductsByType: searchProductsByType,
+          searchAllProducts: searchAllProducts
+      }
+
+      //function definitions go here
+
+  }
+
+  if(typeof(window.api) === 'undefined'){
+      window.api = myLibrary();
+  }
+
+})(window); 
