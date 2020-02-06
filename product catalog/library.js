ab127b3f6cf4954958c3f3fa454729a2a93ad36a
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-07 00:06:08 +0100

    added createRandomCatalog function

--- product catalog/library.js
+++ product catalog/library.js
@@ -12,12 +12,20 @@
       }
 
       //function definitions go here
-      const createRandomProduct = () => {
-        const typeArray = ['Electronics','Book','Clothing','Food'];
-        const price = (Math.random()*500).toFixed(2) 
-        const type = typeArray[Math.floor(Math.random()*4)];
-    
-        return {price:price, type:type};                
+    const createRandomProduct = () => {
+      const typeArray = ['Electronics','Book','Clothing','Food'];
+      const price = (Math.random()*500).toFixed(2) 
+      const type = typeArray[Math.floor(Math.random()*4)];
+  
+      return {price:price, type:type};                
+    }
+    const createRandomCatalog = num => {
+      const catalog = [];
+      for (let i = 0; i < num; i++){
+          const obj = createRandomProduct();
+          catalog.push({id:i,price:obj.price,type:obj.type});
+      }
+      return catalog;
     }
   }
 
