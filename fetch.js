23f0ba12a5f6dbc5858da1a32c29fd35a4eebd42
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-07 13:50:47 +0100

    body attribute

--- fetch.js
+++ fetch.js
@@ -1,17 +1,22 @@
 /* 
-Method
-The method attribute is a string that is used to specify the HTTP request method type. 
-Here is a list of some commonly used method types:
+Body
+The body attribute is a JSON string used to send data along with a fetch request. 
+If the body value is an object, it is important to stringify the object 
+that is being sent using JSON.stringify() 
+or it will not process correctly.
 
-Get - used to retrieve an existing data resource
-Head - used to retrieve HTTP headers
-Post - used to create a new data resource
-Put - used to create a new data resource or modify an existing data resource
-Delete - used to delete a data resource
+Get and Head HTTP requests can not have bodies.
 
-Notice how an init object with a method attribute of "Post" can be created:
+Notice how an init object with a body attribute representing an object can be created:
 */
 
+const myBody = {
+    id: 12345,
+    name: 'abc',
+    age: 21
+};
+
+
 const initObject = {
-  method: 'POST'
-}
+    body: JSON.stringify(myBody)
+};
