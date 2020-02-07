46f167683ba366bdbe9096046da50c0b8f0b5aaa
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-07 11:04:04 +0100

    minor changes (like arrow func)  in functions definitions

--- product catalog/productCatalog.js
+++ product catalog/productCatalog.js
@@ -1,9 +1,9 @@
 const createTableHeader = tableId => {
-  const tableHeaderRow = document.createElement('TR');
-  const th1 = document.createElement('TH');
-  const th2 = document.createElement('TH');
-  const th3 = document.createElement('TH');
-  const th4 = document.createElement('TH');
+  const tableHeaderRow = document.createElement('tr');
+  const th1 = document.createElement('th');
+  const th2 = document.createElement('th');
+  const th3 = document.createElement('th');
+  const th4 = document.createElement('th');
   th1.appendChild(document.createTextNode("ProductId"));
   th2.appendChild(document.createTextNode("Type"));
   th3.appendChild(document.createTextNode("Price"));
@@ -19,30 +19,30 @@ const updateTable = (tableId,productArray) => {
   const tableBody = document.getElementById(tableId);
   //reset table
   while (tableBody.hasChildNodes()) {   
-      tableBody.removeChild(tableBody.firstChild);
+    tableBody.removeChild(tableBody.firstChild);
   }
   //create table header
   createTableHeader(tableId);
   //populate table rows
   for (i = 0; i < productArray.length; i++) {
-      const tr = document.createElement('TR');
-      const td1 = document.createElement('TD');
-      const td2 = document.createElement('TD');
-      const td3 = document.createElement('TD');
-      const td4 = document.createElement('button');
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+    const td4 = document.createElement('button');
 
-      td4.addEventListener('click',function(){
-          
-      });
-      td1.appendChild(document.createTextNode(productArray[i].id));
-      td2.appendChild(document.createTextNode(productArray[i].type));
-      td3.appendChild(document.createTextNode(productArray[i].price));
-      td4.appendChild(document.createTextNode("Examine"));
-      tr.appendChild(td1);
-      tr.appendChild(td2);
-      tr.appendChild(td3);
-      tr.appendChild(td4);
-      tableBody.appendChild(tr);
+    td4.addEventListener('click',() => {
+        
+    });
+    td1.appendChild(document.createTextNode(productArray[i].id));
+    td2.appendChild(document.createTextNode(productArray[i].type));
+    td3.appendChild(document.createTextNode(productArray[i].price));
+    td4.appendChild(document.createTextNode("Examine"));
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tableBody.appendChild(tr);
   }  
 };
 
