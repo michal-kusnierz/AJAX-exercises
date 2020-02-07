cb4532c2d61e82448030d02053dad9b67bb73dc7
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-07 12:41:21 +0100

    assessment: question10

--- module2/assessment.js
+++ module2/assessment.js
@@ -1,9 +1,8 @@
-//Question 9 Code
+//Question 10 Code
 
-/*
-Which of the following statements are true regarding Promises and Continuation Passing Style(CPS) when chaining asynchronous operations? Select all that apply.
-- Promises require a third party library to be used while CPS does not because it is built into JavaScript
-- It is easier to handle errors with Promises than with CPS     // correct
-- Promises do not have to use callback functions while CPS does
-- Promises are easier to maintain and read than CPS             // incorrect
-*/
+var result = Promise.resolve("123");
+
+console.log(result)
+
+
+// a Promise object is logged
