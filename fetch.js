de42b02672bd3ed49fab6cdc75649cfda27ff9e8
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-07 14:22:56 +0100

    reusing request object

--- fetch.js
+++ fetch.js
@@ -1,30 +1,15 @@
 /*
-The fetch() method can take in a Request object instead of an URL and an init object. 
-The Request constructor takes in the same parameters as the fetch() method: 
-an URL and an optional init object. 
-
-Request objects are used because they make Fetch requests a bit cleaner 
-and also offer a bit more control.
+Request objects can be used more than once in Fetch requests 
+that don't involve bodies(Head,Get)..
 */
 
-const initObject = {
-    method: 'POST',
-    headers: new Headers(),
-    mode: 'cors',
-    body: "{}" 
-};
+const request = new Request("https://jsonplaceholder.typicode.com/todos/1");
 
-//creates a new request object using an URL and an init object
-const request = new Request("https://jsonplaceholder.typicode.com/posts",initObject)
-
-//fetch() method used with a request
 fetch(request).then(result => {     
-    //result contains a Response object
-    return result.json() 
-    //returns a Promise containing JSON data extracted from the Response object
-}).then(result => {
-    console.log(result);
-    //logs Object {id: 101}
-}).catch(err => {
-    console.log(err);
+    console.log(result.status) //logs 200, OK fetch response
+});
+
+//reusing request object 
+fetch(request).then(result => { 
+    console.log(result.status) //logs 200, OK fetch response after reusing request object
 });
