53c419dba67a62ccbd93734d3307b12f1c806a75
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-07 13:48:56 +0100

    method attribute

--- fetch.js
+++ fetch.js
@@ -1,22 +1,17 @@
-//this init object specifies the method, headers, mode and body of the request
+/* 
+Method
+The method attribute is a string that is used to specify the HTTP request method type. 
+Here is a list of some commonly used method types:
+
+Get - used to retrieve an existing data resource
+Head - used to retrieve HTTP headers
+Post - used to create a new data resource
+Put - used to create a new data resource or modify an existing data resource
+Delete - used to delete a data resource
+
+Notice how an init object with a method attribute of "Post" can be created:
+*/
+
 const initObject = {
-  method: 'POST',
-  headers: new Headers(),
-  mode: 'cors',
-  body: "{}" 
+  method: 'POST'
 }
-
-//fetch() method used with an URL endpoint and an init object
-fetch("https://jsonplaceholder.typicode.com/posts",initObject) 
-  .then(result => { //result contains a Response object
-     return result.json() //returns a promise containing JSON data extracted from the Response object
-
-  })
-  .then(result => {
-     console.log(result);
-     //logs Object {id: 101}
-
-  })
-  .catch(err => {
-      console.log(err);
-});
