4b7613b1b4ce525f6ac8b049764dbb44d2965908
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-07 13:33:30 +0100

    extractions methods: json() and text()

--- fetch.js
+++ fetch.js
@@ -1,11 +1,22 @@
+// json() method is used to extract a JSONobject:
 fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then(result => {
-    return result.json();
+    return result.json(); 
+    //returns a promise containing the JSON data extracted from the Response object
   })
   .then(result => {
     console.log(result);
     //logs Object {completed: false, id: 1, title: "delectus aut autem", userId: 1}
+  });
+
+
+// text() method is used to extract a text string:
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then(result => {
+    return result.text(); 
+    //returns a promise containing the text data extracted from the Response object
   })
-  .catch(err => {
-    console.log(err);
+  .then(result => {
+    console.log(result);
+    //logs "{completed: false, id: 1, title: "delectus aut autem", userId: 1}"
   });
