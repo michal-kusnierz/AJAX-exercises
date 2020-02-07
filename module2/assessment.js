afd88303884fbf80261c928ac1c212eb956a4bf9
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-07 12:34:12 +0100

    assessment: question7

--- module2/assessment.js
+++ module2/assessment.js
@@ -1,19 +1,37 @@
-//Question 6 Code
+//Question 7 Code
 
-var myPromise = Promise.resolve(123);
+function promiseSquare(val){
+  return Promise.resolve(val * val);
+}
 
-myPromise.then(function(val){
-    return val - 23; 
+function promiseDouble(val){
+  return Promise.resolve(val + val);
+}
+
+function filterFunction(val){
+  if ( val > 50)
+      return true;
+  else
+      return false;
+}
+
+var myPromise1 = Promise.resolve(500);
+var myPromise2 = promiseSquare(10);
+var myPromise3 = promiseDouble(20);
+
+Promise.all([myPromise1,myPromise2,myPromise3]).then(function(val){
+  var result = val.filter(filterFunction);
+  return result;
+
 }).then(function(val){
-    return val * 2;
-}).then(function(val){
-    return Promise.reject(100);
-}).then(function(val){
-    return val + 1;
-}).then(function(val){
-    return val + 2;
+  var temp = 0;
+  for(var i = 0; i < val.length; i++){
+      temp += val[i]
+  }
+  console.log(temp);
+
 }).catch(function(err){
-    console.log("error: " + err)
+  console.log("Error: " + err)
 });
 
-// 100
+// output to the console: 600
