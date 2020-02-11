cf7ff37447947d79951dca8b1e5e57ee635d9b61
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-11 13:59:26 +0100

    request body variable added

--- text analytics/textAnalytics.js
+++ text analytics/textAnalytics.js
@@ -1,3 +1,13 @@
 document.getElementById("analyseBtn").addEventListener("click", analyse);
 
-function analyse(){};
+let reqBody = {
+  "documents": [
+    {
+    "language":"en",
+    "id" : 1,
+    "text": document.getElementById("analyseInput").value
+    }
+  ]
+};
+
+const analyse = () => {};
