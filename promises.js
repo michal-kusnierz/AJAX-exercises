7a012da218c4b5ede37d85e1ce770eab7c319794
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-11 12:43:16 +0100

    change style of paragraph to visible  on button click

--- promises.js
+++ promises.js
@@ -1,11 +1,11 @@
 const promiseDiv = document.getElementById('promise');
 const promiseBtn = document.getElementById('promiseBtn');
-const promiseDisplay = document.createElement('p');
+const promiseDisplay = document.getElementById('promiseDisplay');
     
 const promiseFun = () => {
   const promise = new Promise((resolve, reject) => {
-    promiseDisplay.innerHTML='waiting...';
-    promiseDiv.appendChild(promiseDisplay);
+    promiseDisplay.innerHTML='waiting for random value...';
+    promiseDisplay.style.visibility = 'visible';
 
     setTimeout(function(){
       const val = Math.random();
