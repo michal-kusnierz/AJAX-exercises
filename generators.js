94130ffcb3bf5551a7dc09bbe021e1d12238d7c7
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-13 22:18:24 +0100

    iterating through a generator object with next()

--- generators.js
+++ generators.js
@@ -1,3 +1,13 @@
+/* Generator functions are initially paused and the first call to next()
+ starts the Generator function. 
+The Generator function then runs until it hits the first yield keyword and then pauses.
+ Subsequent calls to next()  will resume the Generator function until the next yield keyword appears.
+
+The next() method returns an object with two properties:
+- done - a boolean indicating whether the Generator function has processed 
+all of the yield statements or has already returned. 
+- value - the value associated with the most recent yield statement. */
+
 function* genFunc() {
   console.log("started");
   yield 'a';
@@ -11,3 +21,12 @@ function* genFunc() {
 }
 
 var genObject = genFunc(); //creates a generator object called genObject
+
+var a = genObject.next(); // Object {value: 'a', done: false}
+//console.log("started");
+
+var b = genObject.next(); // Object {value: undefined, done: false}
+//console.log("passed first yield"); 
+
+var c = genObject.next(); // Object {value: 123, done: false}
+//console.log("passed second yield");
