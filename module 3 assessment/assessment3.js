64bc650bcffcaec3848802de669331ea8b717213
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-13 21:24:59 +0100

    assessment3 - question2

--- module 3 assessment/assessment3.js
+++ module 3 assessment/assessment3.js
@@ -1,10 +1,13 @@
-// Question 1 Code
+// Question 2 Code
 
 fetch("https://jsonplaceholder.typicode.com/todos/15").then(function(val){
-    console.log(val);
+    return val.json();
+}).then(function(val){
+    console.log([val.title]);
 }).catch(function(err){
     console.log("Error")
 });;
 
-// answer: A Response object will be logged
 
+// answer: [INSERT 1] = val.json(),[INSERT 2] = val.title
+// → the code output "ab voluptatum amet voluptas"
