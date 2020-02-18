aa6a27f07708f983f307bfae00b9ab3398621348
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-18 16:48:54 +0100

    modify values using next() method

--- generators.js
+++ generators.js
@@ -1,25 +1,19 @@
 /*
-next() can also be used to send values back into Generator functions. 
-This is accomplished by passing a value into the next() method call as an argument. 
-The value that is passed into the next() method call eventually 
-becomes the return value of the most recent yield statement.
- Since the first next() call starts the Generator function, 
- any value that gets passed into it will be ignored.
+The next() method can also be used to modify the values sent by the yield statement and send them back.
 */
 
- function* genFunc(){
-  var a = yield;
-  console.log(a); //a = 1
-  var b = yield;  
-  console.log(b); //b = 2
-  var c = yield;
-  console.log(c); //c = 3
-
+function* genFunc(){
+  var a = yield 'a';
+  console.log(a); // a = 'a!'
+  var b = yield 'b';  
+  console.log(b); // b = 'B'
+  var c = yield 'c';
+  console.log(c); // c = 'abc'
 }
 
 var genObject = genFunc();
 
-genObject.next(0); //starts genFunc(), the value inside the next() call is ignored
-genObject.next(1); //sends a value of 1 to genFunc()
-genObject.next(2); //sends a value of 2 to genFunc()
-genObject.next(3); //sends a value of 3 to genFunc()
+var w = genObject.next(); //starts genFunc(), w = Object {value: 'a', done: false}
+var x = genObject.next(w.value + '!'); //sends a value of "a!" to genFunc(), x = Object {value: 'b', done: false}
+var y = genObject.next(x.value.toUpperCase()); //sends a value of 'B' to genFunc(), y = Object {value: 'c', done: false}
+var z = genObject.next(w.value + x.value + y.value); //sends a value of 'abc' to genFunc(), z = Object {value: 'undefined', done: true}
