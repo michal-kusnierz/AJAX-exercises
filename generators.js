31ad249bf4f13edacc8ed4ce799387cd08f6caf1
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-19 22:40:39 +0100

    erased comments and fixed fully ES6 syntax

--- generators.js
+++ generators.js
@@ -4,45 +4,38 @@ If a rejected Promise is yielded, the run() method will stop iterating through t
 */
 
 const run = genFunc => {
-  const genObject = genFunc(); //creating a generator object
+  const genObject = genFunc(); 
 
-  function iterate(iteration) {
-    //recursive function to iterate through promises
+  const iterate = iteration => {
     if (iteration.done)
-      //stop iterating when done and return the final value wrapped in a promise
       return Promise.resolve(iteration.value);
-    return Promise.resolve(iteration.value) //returns a promise with its then() and catch() methods filled
-      .then(x => iterate(genObject.next(x))) //calls recursive function on the next value to be iterated
-      .catch(x => iterate(genObject.throw(x))); //throws an error if a rejection is encountered
+    return Promise.resolve(iteration.value)
+      .then(x => iterate(genObject.next(x)))
+      .catch(x => iterate(genObject.throw(x)));
   }
 
   try {
-    return iterate(genObject.next()); //starts the recursive loop
+    return iterate(genObject.next());
   } catch (ex) {
-    return Promise.reject(ex); //returns a rejected promise if an exception is caught
+    return Promise.reject(ex);
   }
 };
 
-
 function *gen2(){
-
   const post1Stream = yield fetch("https://jsonplaceholder.typicode.com/posts/1");
   const post1 = yield post1Stream.json();
   console.log(post1.title); 
-  //post1.title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
   const post2Stream = yield fetch("https://jsonplaceholder.typicode.com/posts/2");
   const post2 = yield post2Stream.json();
   console.log(post2.title);
-  //post2.title = "qui est esse"
 
   const error = yield Promise.reject(Error("error message!"));
-  //error thrown here, generator function terminates
 
   const number = yield 12345;
-  console.log(number); //doesn't occur because an earlier promise was rejected 
+  console.log(number);
 
-  return 'done'; //doesn't occur because an earlier promise was rejected
+  return 'done';
 }
 
 run(gen2).then(x => console.log(x))
-    .catch(err => console.log(err.message)); //logs "error message!" from the rejected Promise
+  .catch(err => console.log(err.message));
