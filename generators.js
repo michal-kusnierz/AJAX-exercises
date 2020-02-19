0f4ba025f5b8876c24b424b64d8c95d7e9b7abfd
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-19 22:24:56 +0100

    run() function used to process generator function

--- generators.js
+++ generators.js
@@ -24,3 +24,35 @@ const run = genFunc => {
     return Promise.reject(ex); //returns a rejected promise if an exception is caught
   }
 };
+
+function *gen(){
+
+  const post1Stream = yield fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const post1 = yield post1Stream.json();
+  console.log(post1.title); 
+  //post1.title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+  const post2Stream = yield fetch("https://jsonplaceholder.typicode.com/posts/2");
+  const post2 = yield post2Stream.json();
+  console.log(post2.title);
+  //post2.title = "qui est esse"
+
+  const number = yield 12345;
+  console.log(number)
+  //number = 12345
+
+  const string = yield "abc";
+  console.log(string)
+  //string = "abc"
+
+  const obj = yield {id:123,name:"xyz"};
+  console.log(obj)
+  //obj = Object {id:123,name:"xyz"}
+
+  const a = yield 54434337746;
+  console.log(a);
+  return "done";
+
+}
+
+run(gen).then(x => console.log(x)) //logs "done"
+  .catch(x => console.log(x.message));
