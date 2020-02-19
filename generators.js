5352bd34d540f5fb250a9ced0b80f7a5f8292d5e
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-19 20:43:49 +0100

    a return() method of generator object

--- generators.js
+++ generators.js
@@ -1,22 +1,25 @@
 /*
-destructuring assignment used to iterate through a Generator object:
+Generator Objects have a return() method that terminates the Generator function. 
+Return() causes a return statement to be performed at the most recent yield statement. 
+The return() method takes in one optional variable that is used as the return value 
+of the Generator function. 
+Calling return(x) will return an object with a value property equal to x 
+and a done property of true. 
+After return() is called, subsequent yield statements in the Generator function are ignored. 
 */
 
 function* genFunc(){
   yield 'a';
-  yield;  
-  yield* [1,2,3];
-  yield 123;
-
+  yield 'b'
+  yield 'c'
   return "finished";
-
 }
 
-var [a,b,c,d,e,f,g] = genFunc(); //destructuring assignment
-// a = 'a'
-// b = undefined
-// c = 1
-// d = 2
-// e = 3
-// f = 123
-// g = undefined <-- g is undefined because there are no more yields
+var genObject = genFunc();
+
+var a = genObject.next(); // a = Object {value: 'a', done: false}
+var b = genObject.return('return() was called'); // b = Object {value: "return() was called", done: true}
+var c = genObject.next(); // c = Object {value: undefined, done: true}
+
+console.log(b.value)
+console.log(c.done)
