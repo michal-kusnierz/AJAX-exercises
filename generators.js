c28dd58dbe600e6bab4dd3aa7f2ea8a61049d351
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-22 00:03:45 +0100

    assessment4 - question1

--- generators.js
+++ generators.js
@@ -1,41 +1,15 @@
-/*
-If a rejected Promise is yielded, the run() method will stop iterating through the Generator function
- and return a rejected Promise.
-*/
-
-const run = genFunc => {
-  const genObject = genFunc(); 
-
-  const iterate = iteration => {
-    if (iteration.done)
-      return Promise.resolve(iteration.value);
-    return Promise.resolve(iteration.value)
-      .then(x => iterate(genObject.next(x)))
-      .catch(x => iterate(genObject.throw(x)));
-  }
-
-  try {
-    return iterate(genObject.next());
-  } catch (ex) {
-    return Promise.reject(ex);
-  }
-};
-
-function *gen2(){
-  const post1Stream = yield fetch("https://jsonplaceholder.typicode.com/posts/1");
-  const post1 = yield post1Stream.json();
-  console.log(post1.title); 
-  const post2Stream = yield fetch("https://jsonplaceholder.typicode.com/posts/2");
-  const post2 = yield post2Stream.json();
-  console.log(post2.title);
-
-  const error = yield Promise.reject(Error("error message!"));
-
-  const number = yield 12345;
-  console.log(number);
-
-  return 'done';
+function* generatorFunction(){
+  yield 1;
+  var a = 2;
+  yield 3;
+  var b = 4;
+  return 5;
 }
 
-run(gen2).then(x => console.log(x))
-  .catch(err => console.log(err.message));
+var generatorObject = generatorFunction();
+
+var a = generatorObject.next();
+var b = generatorObject.next();
+console.log(b.value);
+
+// output: 3
