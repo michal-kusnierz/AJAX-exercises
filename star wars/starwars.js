f70edefe1c91b56f412b7fb6238b80d3566a533e
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-21 21:18:16 +0100

    fetch with the number of film specified

--- star wars/starwars.js
+++ star wars/starwars.js
@@ -1,7 +1,9 @@
 function* gen() {
   if(document.getElementById("input").value > 7 || document.getElementById("input").value < 1 ){
     throw new Error("Invalid Input - Enter a number between 1 and 7");
-}
+  }
+  const filmResponse = yield fetch("http://swapi.co/api/films/" + document.getElementById("input").value);
+  const film = yield filmResponse.json();
 }
 
 const run = genFunc => {
