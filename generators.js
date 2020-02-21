6dc15ded13d93c059045cc0dfac804fdeb55ed23
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-22 00:11:35 +0100

    assessment4 - question 10

--- generators.js
+++ generators.js
@@ -1,13 +1,33 @@
 function* genFunc(){
-  var a = yield 'hello';
-  var b = yield 'world';
-  return "goodbye";
+  var a = yield 8;
+  var b = yield fetch("https://jsonplaceholder.typicode.com/comments/"  + a);
+  var c = yield b.json();
+  var d = c.postId;
+  var e = yield fetch("https://jsonplaceholder.typicode.com/posts/"  + d);
+  var f = yield e.json();
+  var g = f.body;
+  console.log(g);
 }
 
-var genObject = genFunc();
+run(genFunc);
 
-genObject.next();
-var result = genObject.return("done");
-console.log(result.value);
+function run(genFunc){
+  const genObject = genFunc();
 
-// output: 'done'
+  function iterate(iteration){
+      if(iteration.done)
+          return Promise.resolve(iteration.value);
+      return Promise.resolve(iteration.value)
+                    .then( x => iterate(genObject.next(x)))
+                    .catch( x => iterate(genObject.throw(x)));
+
+  }
+  try{
+      return iterate(genObject.next());
+  }
+  catch(ex){
+      return Promise.reject(ex);
+  }
+}
+
+// output: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla'
