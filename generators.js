9bbf4cdf4c33ae04223641eede698f10dcfb408e
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-22 00:10:50 +0100

    assessment4 - question 9

--- generators.js
+++ generators.js
@@ -1,42 +1,13 @@
-function delayedPromiseDouble(val) {
-  var promise = new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      resolve(val + val);
-    }, 2000);
-  });
-  return promise;
-}
-
-function delayedPromiseSquare(val) {
-  var promise = new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      resolve(val * val);
-    }, 2000);
-  });
-  return promise;
-}
-
-function* genFunc() {
-  var a = yield delayedPromiseDouble(5);
-  var b = yield delayedPromiseSquare(a);
-  console.log(a + "," + b);
+function* genFunc(){
+  var a = yield 'hello';
+  var b = yield 'world';
+  return "goodbye";
 }
 
 var genObject = genFunc();
 
-function run(gen) {
-  var genObject = gen();
-  var temp = genObject.next();
-  var promise = temp.value;
-  promise.then(function(val) {
-    var temp2 = genObject.next(val);
-    var secondPromise = temp2.value;
-    secondPromise.then(function(val) {
-      genObject.next(val);
-      return val;
-    });
-  });
-}
+genObject.next();
+var result = genObject.return("done");
+console.log(result.value);
 
-run(genFunc);
-// output: 10,100
+// output: 'done'
