c421be93e712f32da1bb39d137ece086227dec1c
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-21 21:14:48 +0100

    run function

--- star wars/starwars.js
+++ star wars/starwars.js
@@ -1,2 +1,18 @@
-function *gen(){};
-const run = () =>{};
+function* gen() {}
+
+const run = genFunc => {
+  const genObject = genFunc();
+
+  const iterate = iteration => {
+    if (iteration.done) return Promise.resolve(iteration.value);
+    return Promise.resolve(iteration.value)
+      .then(x => iterate(genObject.next(x)))
+      .catch(x => iterate(genObject.throw(x)));
+  };
+
+  try {
+    return iterate(genObject.next());
+  } catch (ex) {
+    return Promise.reject(ex);
+  }
+};
