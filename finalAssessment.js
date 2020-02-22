9b30b28f760a422372708df7e159230b4be7fdad
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-22 13:55:30 +0100

    final - question 8

--- finalAssessment.js
+++ finalAssessment.js
@@ -1,17 +1,25 @@
-const promise = Promise.reject("failure");
+const promiseIndex = (val,index) => {
+   return Promise.resolve(val[index]);
+};
 
-promise.then(val => {
-    console.log("good");
-  })
-  .catch(err => {
-    console.log("bad");
-    return err;
-  })
-  .then(val => {
-    console.log(val);
-  });
+const promiseArray = x => {
+   return Promise.resolve([x,x+1,x+2]);
+};
 
-/* output: 
-bad
-failure
-*/
+const myPromise = Promise.resolve([1,3,5,7]);
+
+myPromise.then(val => {
+   return  val.map(x => x + x);
+}).then(val => {
+   return promiseIndex(val,2);
+}).then(val => {
+   return val + 5;
+}).then(val => {
+   return promiseArray(val);
+}).then(val => {
+   return promiseIndex(val,1);
+}).then(val => {
+   console.log(val);
+});
+
+// output: 16
