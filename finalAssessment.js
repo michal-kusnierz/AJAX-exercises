55ce263769c1706ece5258e7fa892a1f02b28378
michal-kusnierz <kusnierz.michal@gmail.com>
2020-02-22 13:31:15 +0100

    final - question 7

--- finalAssessment.js
+++ finalAssessment.js
@@ -1,34 +1,17 @@
-const timedReject = (val, time) => {
-  const promise = new Promise(function(resolve, reject) {
-    setTimeout(() => {
-      reject(val);
-    }, time);
-  });
-  return promise;
-};
+const promise = Promise.reject("failure");
 
-const timedResolve = (val, time) => {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(val);
-    }, time);
-  });
-  return promise;
-};
-const myPromise1 = timedReject(1, 200);
-const myPromise2 = timedResolve(2, 400);
-const myPromise3 = timedResolve(3, 300);
-
-const myPromise4 = Promise.all([myPromise1, myPromise2]).then(() => {
-  return 4;
-});
-
-Promise.race([myPromise3, myPromise4])
-  .then(val => {
-    console.log(val);
+promise.then(val => {
+    console.log("good");
   })
   .catch(err => {
-    console.log("Error: " + err);
+    console.log("bad");
+    return err;
+  })
+  .then(val => {
+    console.log(val);
   });
 
-// output: Error: 1
+/* output: 
+bad
+failure
+*/
